c7a4be873def792525db72f049844ff4e227ec23
Andrew Carr <andrew@foxglove.dev>
2022-08-24 15:03:11 +1000

    remove use of useTooltip from PanelList

--- packages/studio-base/src/components/PanelList/index.tsx
+++ packages/studio-base/src/components/PanelList/index.tsx
@@ -19,13 +19,14 @@ import {
   CardContent,
   CardMedia,
   Container,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
-  Typography,
   TextField,
-  IconButton,
+  Tooltip,
+  Typography,
 } from "@mui/material";
 import fuzzySort from "fuzzysort";
 import { countBy, isEmpty } from "lodash";
@@ -36,7 +37,6 @@ import { makeStyles } from "tss-react/mui";
 
 import Stack from "@foxglove/studio-base/components/Stack";
 import TextHighlight from "@foxglove/studio-base/components/TextHighlight";
-import { useTooltip } from "@foxglove/studio-base/components/Tooltip";
 import {
   useCurrentLayoutActions,
   usePanelMosaicId,
@@ -173,33 +173,12 @@ function DraggablePanelItem({
     }
   }, [highlighted]);
 
-  const { ref: tooltipRef, tooltip } = useTooltip({
-    contents:
-      mode === "grid" ? (
-        panel.description
-      ) : (
-        <Stack style={{ width: 200 }}>
-          {panel.thumbnail != undefined && <img src={panel.thumbnail} alt={panel.title} />}
-          <Stack padding={1} gap={0.5}>
-            <Typography variant="body2" style={{ fontWeight: "bold" }}>
-              {panel.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {panel.description}
-            </Typography>
-          </Stack>
-        </Stack>
-      ),
-    placement: mode === "grid" ? undefined : "right",
-    delay: 200,
-  });
   const mergedRef = useCallback(
     (el: HTMLElement | ReactNull) => {
       connectDragSource(el);
-      tooltipRef(el);
       scrollRef.current = el;
     },
-    [connectDragSource, tooltipRef, scrollRef],
+    [connectDragSource, scrollRef],
   );
 
   const targetString = panel.extensionNamespace
@@ -210,48 +189,66 @@ function DraggablePanelItem({
     case "grid":
       return (
         <Card className={classes.fullHeight}>
-          <CardActionArea ref={mergedRef} onClick={onClick} className={classes.fullHeight}>
-            <Stack fullHeight>
-              {panel.thumbnail != undefined ? (
-                <CardMedia component="img" image={panel.thumbnail} alt={panel.title} />
-              ) : (
-                <div className={classes.imagePlaceholder} />
-              )}
-              <CardContent className={classes.cardContent}>
-                <Typography variant="subtitle2" gutterBottom>
-                  <span data-testid={`panel-menu-item ${panel.title}`}>
-                    <TextHighlight targetStr={targetString} searchText={searchQuery} />
-                  </span>
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <TextHighlight targetStr={panel.description ?? ""} searchText={searchQuery} />
-                </Typography>
-              </CardContent>
-            </Stack>
-          </CardActionArea>
+          <Tooltip
+            enterDelay={200}
+            title={
+              <Stack style={{ width: 200 }}>
+                {panel.thumbnail != undefined && <img src={panel.thumbnail} alt={panel.title} />}
+                <Stack padding={1} gap={0.5}>
+                  <Typography variant="body2" style={{ fontWeight: "bold" }}>
+                    {panel.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {panel.description}
+                  </Typography>
+                </Stack>
+              </Stack>
+            }
+          >
+            <CardActionArea ref={mergedRef} onClick={onClick} className={classes.fullHeight}>
+              <Stack fullHeight>
+                {panel.thumbnail != undefined ? (
+                  <CardMedia component="img" image={panel.thumbnail} alt={panel.title} />
+                ) : (
+                  <div className={classes.imagePlaceholder} />
+                )}
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="subtitle2" gutterBottom>
+                    <span data-testid={`panel-menu-item ${panel.title}`}>
+                      <TextHighlight targetStr={targetString} searchText={searchQuery} />
+                    </span>
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    <TextHighlight targetStr={panel.description ?? ""} searchText={searchQuery} />
+                  </Typography>
+                </CardContent>
+              </Stack>
+            </CardActionArea>
+          </Tooltip>
         </Card>
       );
 
     case "list":
       return (
-        <ListItem disableGutters disablePadding selected={highlighted}>
-          {tooltip}
-          <ListItemButton
-            className={classes.grab}
-            disabled={checked}
-            ref={mergedRef}
-            onClick={onClick}
-          >
-            <ListItemText
-              primary={
-                <span data-testid={`panel-menu-item ${panel.title}`}>
-                  <TextHighlight targetStr={targetString} searchText={searchQuery} />
-                </span>
-              }
-              primaryTypographyProps={{ fontWeight: checked ? "bold" : undefined }}
-            />
-          </ListItemButton>
-        </ListItem>
+        <Tooltip title={panel.description ?? panel.title} placement="right">
+          <ListItem disableGutters disablePadding selected={highlighted}>
+            <ListItemButton
+              className={classes.grab}
+              disabled={checked}
+              ref={mergedRef}
+              onClick={onClick}
+            >
+              <ListItemText
+                primary={
+                  <span data-testid={`panel-menu-item ${panel.title}`}>
+                    <TextHighlight targetStr={targetString} searchText={searchQuery} />
+                  </span>
+                }
+                primaryTypographyProps={{ fontWeight: checked ? "bold" : undefined }}
+              />
+            </ListItemButton>
+          </ListItem>
+        </Tooltip>
       );
   }
 }
