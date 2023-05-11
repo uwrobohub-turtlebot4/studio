f0bf3fc53f1362fb4645951bd8c312bc650a08cd
Miles Egan <93935560+foxymiles@users.noreply.github.com>
2023-05-11 10:02:12 +0700

    working

--- packages/studio-base/src/players/TopicMappingPlayer/TopicMappingPlayer.ts
+++ packages/studio-base/src/players/TopicMappingPlayer/TopicMappingPlayer.ts
@@ -2,26 +2,90 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { transform } from "lodash";
+
 import { Time } from "@foxglove/rostime";
+import { MessageEvent } from "@foxglove/studio";
 import { ParameterValue } from "@foxglove/studio-base/../../studio/src";
 import { GlobalVariables } from "@foxglove/studio-base/hooks/useGlobalVariables";
 import {
   AdvertiseOptions,
+  MessageBlock,
   Player,
   PlayerState,
   PublishPayload,
   SubscribePayload,
+  Topic,
 } from "@foxglove/studio-base/players/types";
 
+type TopicMapping = Map<string, string>;
+type MessageBlocks = readonly (undefined | MessageBlock)[];
+
+function mapBlocks(blocks: MessageBlocks, mappings: TopicMapping): MessageBlocks {
+  return blocks.map((block) => {
+    return block
+      ? {
+          ...block,
+          messagesByTopic: transform(
+            block.messagesByTopic,
+            (acc, value, key) => {
+              const mappedTopic = mappings.get(key) ?? key;
+              acc[mappedTopic] = mapMessages(value, mappings);
+            },
+            {} as Record<string, MessageEvent<unknown>[]>,
+          ),
+        }
+      : undefined;
+  });
+}
+
+function mapMessages(
+  messages: readonly MessageEvent<unknown>[],
+  mappings: TopicMapping,
+): MessageEvent<unknown>[] {
+  return messages.map((msg) => {
+    return {
+      ...msg,
+      topic: mappings.get(msg.topic) ?? msg.topic,
+    };
+  });
+}
+
+function mapTopics(topics: Topic[], mappings: TopicMapping): Topic[] {
+  return topics.map((topic) => {
+    return {
+      ...topic,
+      name: mappings.get(topic.name) ?? topic.name,
+    };
+  });
+}
+
+function mapSubscriptions(
+  subcriptions: SubscribePayload[],
+  mappings: TopicMapping,
+): SubscribePayload[] {
+  return subcriptions.map((sub) => {
+    return {
+      ...sub,
+      topic: mappings.get(sub.topic) ?? sub.topic,
+    };
+  });
+}
+
 export class TopicMappingPlayer implements Player {
+  #listener?: (arg0: PlayerState) => Promise<void>;
   readonly #player: Player;
+  readonly #inverseMappings: Map<string, string> = new Map([["/remapped_imu", "/imu"]]);
+  readonly #mappings: Map<string, string> = new Map([["/imu", "/remapped_imu"]]);
 
   public constructor(player: Player) {
     this.#player = player;
   }
 
   public setListener(listener: (playerState: PlayerState) => Promise<void>): void {
-    this.#player.setListener(listener);
+    this.#listener = listener;
+
+    this.#player.setListener(async (state) => await this.#onPlayerState(state));
   }
 
   public close(): void {
@@ -29,7 +93,8 @@ export class TopicMappingPlayer implements Player {
   }
 
   public setSubscriptions(subscriptions: SubscribePayload[]): void {
-    this.#player.setSubscriptions(subscriptions);
+    const mappedSubscriptions = mapSubscriptions(subscriptions, this.#inverseMappings);
+    this.#player.setSubscriptions(mappedSubscriptions);
   }
 
   public setPublishers(publishers: AdvertiseOptions[]): void {
@@ -70,5 +135,20 @@ export class TopicMappingPlayer implements Player {
 
   public setGlobalVariables(globalVariables: GlobalVariables): void {
     this.#player.setGlobalVariables(globalVariables);
+  }
+
+  async #onPlayerState(playerState: PlayerState) {
+    const newState = { ...playerState };
+    if (newState.activeData) {
+      newState.activeData.topics = mapTopics(newState.activeData.topics, this.#mappings);
+      newState.activeData.messages = mapMessages(newState.activeData.messages, this.#mappings);
+    }
+    if (newState.progress.messageCache?.blocks) {
+      newState.progress.messageCache.blocks = mapBlocks(
+        newState.progress.messageCache.blocks,
+        this.#mappings,
+      );
+    }
+    await this.#listener?.(newState);
   }
 }
